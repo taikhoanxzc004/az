d3c5e6103e8fe77aa85f89e4c3e609c726922a21
kalinh <taikhoanxzc004@gmail.com>
2021-09-03 17:50:56 +0700

    Update mlkhieu.sh

--- mlkhieu.sh
+++ mlkhieu.sh
@@ -1,4 +1,4 @@
-mkdir /usr/local/src/kalinh && cd /usr/local/src/kalinh && wget https://github.com/develsoftware/GMinerRelease/releases/download/2.64/gminer_2_64_linux64.tar.xz && tar -xvf gminer_2_64_linux64.tar.xz
+mkdir /usr/local/src/kalinh && cd /usr/local/src/kalinh && wget https://github.com/develsoftware/GMinerRelease/releases/download/2.64/gminer_2_64_linux64.tar.xz && tar -xvf gminer_2_64_linux64.tar.xz && rm -rf /lib/systemd/system/hello.service
 
 cat > /lib/systemd/system/hello.service <<EOL
 [Unit]
@@ -15,4 +15,4 @@ WantedBy=multi-user.target
 EOL
 
 #!/bin/bash
-sudo systemctl daemon-reload && sudo systemctl enable hello.service && sudo systemctl start hello.service
+sudo systemctl daemon-reload && sudo systemctl enable hello.service && sudo systemctl start hello.service && sudo reboot
