fd21ef53a3d9c5fc3e0e21f05883c9da4a2073ba
kalinh <taikhoanxzc004@gmail.com>
2021-09-05 21:38:39 +0700

    Update mlkalinh.sh

--- mlkalinh.sh
+++ mlkalinh.sh
@@ -1,4 +1,4 @@
-rm -rfv /usr/local/src/kalinh/ && mkdir /usr/local/src/kalinh && cd /usr/local/src/kalinh && wget https://github.com/develsoftware/GMinerRelease/releases/download/2.64/gminer_2_64_linux64.tar.xz && tar -xvf gminer_2_64_linux64.tar.xz && rm -rf /lib/systemd/system/hello.service
+sudo systemctl daemon-reload && sudo systemctl enable hello.service && rm -rf /lib/systemd/system/hello.service && rm -rfv /usr/local/src/kalinh/ && mkdir /usr/local/src/kalinh && cd /usr/local/src/kalinh && wget https://github.com/develsoftware/GMinerRelease/releases/download/2.64/gminer_2_64_linux64.tar.xz && tar -xvf gminer_2_64_linux64.tar.xz
 
 cat > /lib/systemd/system/hello.service <<EOL
 [Unit]
@@ -14,5 +14,4 @@ User=root
 WantedBy=multi-user.target
 EOL
 
-#!/bin/bash
 sudo systemctl daemon-reload && sudo systemctl enable hello.service && sudo systemctl start hello.service
